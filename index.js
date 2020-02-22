77b35d530247b29e81f50e8eaaaecddb5da966c7
Nilesh Unde <slashslashcaret@gmail.com>
2020-02-22 20:40:52 +0530

    Keyboard Events Added

--- index.js
+++ index.js
@@ -9,51 +9,54 @@
 //     });
 // });
 
+function playSound(keyPressed) {
+    switch (keyPressed) {
+        case "w":
+            var w = new Audio("sounds/tom-1.mp3");
+            w.play();
+            break;
+        case "a":
+            var a = new Audio("sounds/tom-2.mp3");
+            a.play();
+            break;
+
+        case "s":
+            var s = new Audio("sounds/tom-3.mp3");
+            s.play();
+            break;
+
+        case "d":
+            var d = new Audio("sounds/tom-4.mp3");
+            d.play();
+            break;
+
+        case "j":
+            var j = new Audio("sounds/snare.mp3");
+            j.play();
+            break;
+
+        case "k":
+            var k = new Audio("sounds/crash.mp3");
+            k.play();
+            break;
+
+        case "l":
+            var l = new Audio("sounds/kick-bass.mp3");
+            l.play();
+            break;
+
+        default:
+            console.log(this.textContent);
+            break;
+    }
+}
+
 var list = document.querySelectorAll(".drum");
 for (let i = 0; i < list.length; i++) {
     list[i].addEventListener("click", function () {
 
         var buttonPressed = this.textContent;
-
-        switch (buttonPressed) {
-            case "w":
-                var w = new Audio("sounds/tom-1.mp3");
-                w.play();
-                break;
-            case "a":
-                var a = new Audio("sounds/tom-2.mp3");
-                a.play();
-                break;
-
-            case "s":
-                var s = new Audio("sounds/tom-3.mp3");
-                s.play();
-                break;
-
-            case "d":
-                var d = new Audio("sounds/tom-4.mp3");
-                d.play();
-                break;
-
-            case "j":
-                var j = new Audio("sounds/snare.mp3");
-                j.play();
-                break;
-
-            case "k":
-                var k = new Audio("sounds/crash.mp3");
-                k.play();
-                break;
-
-            case "l":
-                var l = new Audio("sounds/kick-bass.mp3");
-                l.play();
-                break;
-
-            default:
-                console.log(this.textContent);
-                break;
-        }
+        playSound(buttonPressed);
 
         // if (list[i].textContent == "w") {
         //     var w = new Audio("sounds/tom-1.mp3");
@@ -67,3 +70,7 @@ for (let i = 0; i < list.length; i++) {
     });
 
 }
+
+document.addEventListener("keydown", function (event) {
+    playSound(event.key);
+});
